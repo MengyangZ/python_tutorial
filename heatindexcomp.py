56b65da0acd26584f0ac6fe71654db36be882f1d
MengyangZ <47376014+MengyangZ@users.noreply.github.com>
2021-04-22 00:40:44 -0400

    First commit

---
+++ heatindexcomp.py
@@ -0,0 +1,18 @@
+from mysci.readdata import read_data
+from mysci.printing import print_comparison
+from mysci.computation import compute_heatindex
+
+# Column names and column indices to read
+columns = {'date': 0, 'time': 1, 'tempout': 2, 'humout': 5, 'heatindex': 13}
+
+# Data types for each column (only for non-string)
+types = {'tempout': float, 'humout': float, 'heatindex': float}
+
+# Read data from file
+data = read_data(columns, types=types)
+
+# Compute the heat index
+heatindex  = [compute_heatindex(t, hum) for t, hum in zip(data['tempout'], data['humout'])]
+
+# Output comparison of data
+print_comparison('HEAT INDX', data['date'], data['time'], data['heatindex'], heatindex)
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="mysci",
+    version="1.0.0",
+    description="A sample package",
+    author="MengyangZ",
+    author_email="mengyang.zhou@uconn.edu",
+    packages=["mysci"],
+    install_requires=[],
+)
+
+
